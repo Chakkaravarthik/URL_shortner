d2829f0bb9671b22ccddc2d78fd079b4ce8ee304
Chakkaravarthi K <chakkaravarthik99@gmail.com>
2025-02-06 23:54:42 +0530

    login register updated

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -17,48 +17,51 @@ const LoginForm = () => {
     });
   };
 
- const Navigate = useNavigate();
+  const Navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = await userlogin(formData);
-    if(data.code===1){
-        Navigate('/shorten-url')
+    if (data.code === 1) {
+      Navigate('/shorten-url')
     }
-    
+
   };
 
   return (
-    <div className="container mt-5">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form>
-      <Link to='/register'>Register</Link>
+    <div className="container-fluid mt-5 d-flex align-items-center justify-content-center" >
+      <div className='card p-4 shadow-lg' style={{width:'50vw', height:"50vh"}}>
+        <h2 className=''>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">Login</button>
+        </form>
+        <Link to='/register'>Register</Link>
+
+      </div>
     </div>
   );
 };
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -25,48 +25,50 @@ const RegisterForm = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <h2>Register Form</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-      <Link to='/login'>Login</Link>
+    <div className="container-fluid mt-5 d-flex align-items-center justify-content-center">
+      <div className='card p-4 shadow-lg' style={{width:'50vw', height:"50vh"}}>
+        <h2>Register Form</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+        <Link to='/'>Login</Link>
+      </div>
     </div>
   );
 };
